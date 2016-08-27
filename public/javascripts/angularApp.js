5e8991f962a6911d4041f825e388ad13c0655f4b
geobas <ksenera@yahoo.com>
2016-08-28 01:07:16 +0300

    Initial commit

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,294 @@
+var app = angular.module('redditClone', ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'MainCtrl',
+            resolve: {
+                postPromise: ['Posts',
+                    function(Posts) {
+                        return Posts.getAll();
+                    }
+                ]
+            }
+        }).
+        state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl',
+            resolve: {
+                post: ['$stateParams', 'Posts',
+                    function($stateParams, Posts) {
+                        return Posts.getOne($stateParams.id);
+                    }
+                ]
+            }
+        }).
+        state('login', {
+            url: '/login',
+            templateUrl: '/login.html',
+            controller: 'AuthCtrl',
+            onEnter: ['$state', 'Auth',
+                function($state, Auth) {
+                    if (Auth.isLoggedIn()) {
+                        $state.go('home');
+                    }
+                }
+            ]
+        }).
+        state('register', {
+            url: '/register',
+            templateUrl: '/register.html',
+            controller: 'AuthCtrl',
+            onEnter: ['$state', 'Auth',
+                function($state, Auth) {
+                    if (Auth.isLoggedIn()) {
+                        $state.go('home');
+                    }
+                }
+            ]
+        });
+
+        $urlRouterProvider.otherwise('home');
+    }
+]);
+
+app.controller('MainCtrl', ['$scope', 'Posts', 'Auth',
+    function($scope, Posts, Auth) {
+        /*
+		$scope.posts = [{
+			title: 'post 1',
+			upvotes: 5
+		}, {
+			title: 'post 2',
+			upvotes: 2
+		}, {
+			title: 'post 3',
+			upvotes: 15
+		}, {
+			title: 'post 4',
+			upvotes: 9
+		}, {
+			title: 'post 5',
+			upvotes: 4
+		}];
+		*/
+        $scope.posts = Posts.posts;
+
+        $scope.addPost = function() {
+            if (!$scope.title || $scope.title === '') {
+                return;
+            }
+            Posts.createPost({
+                title: $scope.title,
+                link: $scope.link,
+            });
+            $scope.title = '';
+            $scope.link = '';
+        };
+
+        $scope.incrementUpvotes = function(post) {
+            if (!Auth.isLoggedIn())
+                $scope.error = 'You need to be logged in to upvote.';
+            Posts.upVote(post);
+        };
+
+        $scope.isLoggedIn = Auth.isLoggedIn;
+    }
+]);
+
+app.controller('PostsCtrl', ['$scope', 'Posts', 'post', 'Auth',
+    function($scope, Posts, post, Auth) {
+
+        $scope.post = post;
+
+        $scope.addComment = function() {
+            if ($scope.body === '') {
+                return;
+            }
+
+            Posts.addComment(post._id, {
+                body: $scope.body,
+                author: 'user',
+            }).success(function(comment) {
+                $scope.post.comments.push(comment);
+            });
+
+            $scope.body = '';
+        };
+
+        $scope.incrementUpvotes = function(comment) {
+            if (!Auth.isLoggedIn())
+                $scope.error = 'You need to be logged in to upvote.';
+            Posts.upvoteComment(post, comment);
+        };
+
+        $scope.isLoggedIn = Auth.isLoggedIn;
+    }
+]);
+
+app.controller('AuthCtrl', [
+    '$scope',
+    '$state',
+    'Auth',
+    function($scope, $state, Auth) {
+        $scope.user = {};
+
+        $scope.register = function() {
+            Auth.register($scope.user).error(function(error) {
+                $scope.error = error;
+            }).then(function() {
+                $state.go('home');
+            });
+        };
+
+        $scope.logIn = function() {
+            Auth.login($scope.user).error(function(error) {
+                $scope.error = error;
+            }).then(function() {
+                $state.go('home');
+            });
+        };
+    }
+]);
+
+app.controller('NavCtrl', [
+    '$scope',
+    'Auth',
+    function($scope, Auth) {
+        $scope.isLoggedIn = Auth.isLoggedIn;
+        $scope.currentUser = Auth.currentUser;
+        $scope.logOut = Auth.logout;
+    }
+]);
+
+app.factory('Posts', ['$http', 'Auth',
+    function($http, Auth) {
+
+        function getAllPosts() {
+            return $http.get('/posts').success(function(data) {
+                angular.copy(data, o.posts);
+            });
+        };
+
+        function createNewPost(post) {
+            return $http.post('/posts', post, {
+                headers: {
+                    Authorization: 'Bearer ' + Auth.getToken()
+                }
+            }).success(function(data) {
+                o.posts.push(data);
+            });
+        }
+
+        function updatePostVote(post) {
+            return $http.put('/posts/' + post._id + '/upvote', null, {
+                headers: {
+                    Authorization: 'Bearer ' + Auth.getToken()
+                }
+            }).success(function(data) {
+                post.upvotes += 1;
+            });
+        }
+
+        function getPost(id) {
+            return $http.get('/posts/' + id).then(function(res) {
+                return res.data;
+            });
+        }
+
+        function addNewComment(id, comment) {
+            return $http.post('/posts/' + id + '/comments', comment, {
+                headers: {
+                    Authorization: 'Bearer ' + Auth.getToken()
+                }
+            });
+        }
+
+        function updateCommentVote(post, comment) {
+            return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote', null, {
+                headers: {
+                    Authorization: 'Bearer ' + Auth.getToken()
+                }
+            }).success(function(data) {
+                comment.upvotes += 1;
+            });
+        }
+
+        var o = {
+            posts: [],
+            getAll: getAllPosts,
+            getOne: getPost,
+            createPost: createNewPost,
+            upVote: updatePostVote,
+            addComment: addNewComment,
+            upvoteComment: updateCommentVote
+        };
+
+        return o;
+    }
+]);
+
+app.factory('Auth', ['$http', '$window',
+    function($http, $window) {
+
+        function saveToken(token) {
+            $window.localStorage['reddit-clone-token'] = token;
+        }
+
+        function getToken() {
+            return $window.localStorage['reddit-clone-token'];
+        }
+
+        function checkIfUserisLoggedIn() {
+            var token = auth.getToken();
+
+            if (token) {
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.exp > Date.now() / 1000;
+            } else {
+                return false;
+            }
+        };
+
+        function getCurrentUser() {
+            if (auth.isLoggedIn()) {
+                var token = auth.getToken();
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.username;
+            }
+        };
+
+        function registerUser(user) {
+            return $http.post('/register', user).success(function(data) {
+                auth.saveToken(data.token);
+            });
+        };
+
+        function logInUser(user) {
+            return $http.post('/login', user).success(function(data) {
+                auth.saveToken(data.token);
+            });
+        };
+
+        function logOutUser() {
+            $window.localStorage.removeItem('reddit-clone-token');
+        };
+
+        var auth = {
+            saveToken: saveToken,
+            getToken: getToken,
+            isLoggedIn: checkIfUserisLoggedIn,
+            currentUser: getCurrentUser,
+            register: registerUser,
+            login: logInUser,
+            logout: logOutUser
+        };
+
+        return auth;
+    }
+]);
